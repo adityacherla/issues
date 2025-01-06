3195f7a6ee68697ee6c99ce5c409dcde50444b14
reco.lightstar@gmail.com <reco.lightstar@gmail.com>
2025-01-06 16:15:25 +0530

    Added the escript configuration in the mix.exs

--- mix.exs
+++ mix.exs
@@ -4,6 +4,7 @@ defmodule Issues.MixProject do
   def project do
     [
       app: :issues,
+      escript: escript_config(),
       version: "0.1.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
@@ -27,4 +28,11 @@ defmodule Issues.MixProject do
       { :poison, "~> 3.1"},
     ]
   end
+
+  defp escript_config() do
+    [
+      main_module: Issues.CLI
+    ]
+  end
+
 end
